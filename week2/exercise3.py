83dae7d5edab2f55d179cea2f090a8cf3c9da69e
Terry <terry@agapitos.com.au>
2017-03-14 11:27:34 +1100

    is_odd, moves, loops1a

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -9,7 +9,9 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    pass
+    if a_number % 2:
+        return True
+    return False
 
 
 def fix_it(moves=True, should_move=True):
@@ -23,7 +25,12 @@ def fix_it(moves=True, should_move=True):
     "Duct Tape"
     "No Problem"
     """
-    pass
+    if moves and should_move or not (moves and should_move):
+        return "No Problem"
+    elif moves and not should_move:
+        return "Duct Tape"
+    else:
+        return "WD-40"
 
 
 def loops_1a():
@@ -33,7 +40,7 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    pass
+    return ['*' for _ in range(10)]
 
 
 def star_map():
