e0c480376e26a51c827315f6768cdbee3e19e390
TerryAg <terry@agapitos.com.au>
2017-05-27 15:24:05 +1000

    Update odp.py

--- week6/odp.py
+++ week6/odp.py
@@ -1,15 +1,42 @@
 import csv
 import pandas as pd
-from pylab import show
 import matplotlib.pyplot as plt
 import matplotlib
+from collections import Counter
+import os
+import geopandas as gp
+import shapely
+import fiona
 df = pd.read_csv("registeredpets.csv")
-plt.hist(df["age"][df["age"] < 30], 29, facecolor="blue", alpha=0.75)
-plt.xlabel("Age")
-plt.ylabel("Number of pets")
-plt.title("List of all animals and their age")
-plt.grid(True, axis='x', ls='dotted')
-plt.show()
-# Brown: Both
-# Blue: Cats
-# Yellow: Dogs
+print(df.columns)
+
+def display_age_graph():
+	plt.hist(df["age"][df["age"] < 30], 29, facecolor="blue", alpha=0.75)
+	plt.xlabel("Age", fontsize=20)
+	plt.ylabel("Number of pets", fontsize=20)
+	plt.title("List of all animals and their age")
+	plt.grid(True, axis='x', ls=':')
+	plt.show()
+
+def pet_count_suburbs():
+	d = Counter(df["suburb"])
+	suburbs = d.keys()
+	suburbs = map(str.strip, sorted(suburbs, key=lambda x:d[x]))
+	plt.bar(range(len(d)), sorted(d.values()))
+	plt.xticks(map(lambda x: x+0.4, range(len(d))), suburbs, rotation=90, size='xx-small')
+	plt.show()
+
+
+"""print(os.path.isfile("geelongpos/Geelong_POS.shp"))
+burbs = gp.GeoDataFrame.from_file("geelongpos/Geelong_POS.shp")
+print("here")
+plt.savefig('mynewmap.png')
+burbs.plot()
+plt.show()"""
+
+display_age_graph()
+#print(set(map(str.strip, df["suburb"])))
+#map_suburbs()
+#df.head()
+#display_age_graph()
+
