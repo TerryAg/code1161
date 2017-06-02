0e1a342769567b016f7d7710a2bfc703b9d9c7ea
Terry <terry@agapitos.com.au>
2017-06-02 13:13:09 +1000

    more

--- OpenDataProject/dataproject.py
+++ OpenDataProject/dataproject.py
@@ -30,6 +30,14 @@ def pet_count_suburbs():
 	plt.xticks(map(lambda x: x+0.4, range(len(d))), suburbs, rotation=90, size='xx-small')
 	plt.show()
 
+def make_autopct(values):
+	# https://stackoverflow.com/a/6170354/1971805
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+    return my_autopct
+
 def registered_or_not():
 	d = defaultdict(int)
 	for reg in df["registered"]:
@@ -38,7 +46,18 @@ def registered_or_not():
 		elif reg == "F":
 			d["False"] += 1
 		#else:nan
-	plt.pie(map(float, d.values()), labels=d.keys())
+	plt.pie(map(float, d.values()), labels=d.keys(), autopct=make_autopct(d.values()))
+	plt.axis('equal')
+	plt.show()
+
+def dogs_v_cats():
+	d = defaultdict(int)
+	for t in df["type"]:
+		if t == "Dog":
+			d["Dogs"] += 1
+		elif t == "Cat":
+			d["Cats"] += 1
+	plt.pie(map(float, d.values()), labels=d.keys(), autopct=make_autopct(d.values()))
 	plt.axis('equal')
 	plt.show()
 
@@ -62,6 +81,28 @@ def colours():
 	plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 	plt.show()
 	
+def top_pet_names(percentage):
+	"""
+	Returns top percentage% of pet names
+	"""
+	d = Counter(df["animal_nam"])
+	#print(d)
+	#print(d.items())
+	ranked = sorted(d.items(), key=lambda x: x[1], reverse=True)
+	most_common = ranked[:percentage]
+	remaining = 0
+	for i in ranked[percentage:]:
+		remaining += i[1]
+	print(remaining)
+	main = 0
+	for i in most_common:
+		main += i[1]
+	print(main)
+	print("%.2f%%" % (main*1.0/remaining*1.0 * 100.0))
+
+
+
+
 #burbs = gp.GeoDataFrame.from_file("Geelong_Roads.shp")
 #geelong_coords = [38.1499, 144.3617]
 #g=folium.Map(location=geelong_coords, zoom_start=3.6)
@@ -72,7 +113,12 @@ def colours():
 #burbs[burbs["GEOCODE"] is not None].plot()
 #plt.show()
 
-#display_age_graph()
-#pet_count_suburbs()
-#colours()
-registered_or_not()
+# BASICS #
+# display_age_graph()
+# pet_count_suburbs()
+# colours()
+# registered_or_not()
+# dogs_v_cats()
+top_pet_names(50)
+
+
