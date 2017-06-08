ba916aef1eb21fc78736ffe711eafcbe0816a2db
Terry <terry@agapitos.com.au>
2017-06-09 09:26:06 +1000

    powerpoint + cleanup

--- OpenDataProject/dataproject.py
+++ OpenDataProject/dataproject.py
@@ -1,15 +1,17 @@
 import csv
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-import numpy as np
 from collections import Counter, defaultdict
 import os
 df = pd.read_csv("registeredpets.csv")
-print(df.columns)
 fontsize = 20
 
 def display_age_graph():
+	"""
+	Returns a histogram of ages.
+	"""
 	plt.hist(df["age"][df["age"] < 30], 29, facecolor="blue", alpha=0.75)
 	plt.xlabel("Age", fontsize=fontsize)
 	plt.ylabel("Number of pets", fontsize=fontsize)
@@ -18,7 +20,9 @@ def display_age_graph():
 	plt.show()
 
 def pet_count_suburbs():
-	#
+	"""
+	Returns a bar graph of all suburb pet count.
+	"""
 	d = Counter(df["suburb"])
 	suburbs = d.keys()
 	suburbs = map(str.strip, sorted(suburbs, key=lambda x:d[x]))
@@ -27,11 +31,9 @@ def pet_count_suburbs():
 	plt.title("Amount of pets in suburbs")
 	plt.xticks(map(lambda x: x+0.4, range(len(d))), suburbs, rotation=90, size='xx-small')
 	plt.show()
-#do above but dogs/cats separated
-# https://stackoverflow.com/questions/23293011/how-to-plot-a-superimposed-bar-chart-using-matplotlib-in-python
 
 def make_autopct(values):
-	# https://stackoverflow.com/a/6170354/1971805
+	# Source: https://stackoverflow.com/a/6170354/1971805
     def my_autopct(pct):
         total = sum(values)
         val = int(round(pct*total/100.0))
@@ -39,19 +41,24 @@ def make_autopct(values):
     return my_autopct
 
 def registered_or_not():
+	"""
+	Returns a pie chart of registered pets to non-registered.
+	"""
 	d = defaultdict(int)
 	for reg in df["registered"]:
 		if reg == "T":
 			d["True"] += 1
 		elif reg == "F":
 			d["False"] += 1
-		#else:nan
 	plt.pie(map(float, d.values()), labels=d.keys(), autopct=make_autopct(d.values()))
 	plt.title("Number of registered pets")
 	plt.axis('equal')
 	plt.show()
 
 def dogs_v_cats():
+	"""
+	Returns a pie chart of dogs to cats.
+	"""
 	d = defaultdict(int)
 	for t in df["type"]:
 		if t == "Dog":
@@ -64,32 +71,32 @@ def dogs_v_cats():
 	plt.show()
 
 def colours():
+	"""
+	Returns pie chart of most common colours.
+	"""
 	d = defaultdict(int)
 	colour_count = {}
 	#print(df["colour"])
 	for colour in df["colour"]:
-		# if it's a nan
+		# If it's NaN
 		if isinstance(colour, float): continue
 		slash = map(str.strip, colour.split("/"))
-		for c in slash:
-			d[c] += 1
-	d = {k:v for k, v in d.items() if v > 500}
+		for col in slash:
+			d[col] += 1
+	d = {k:v for k, v in d.items() if v > 500} # Only including colours mentioned 500 times or more
 	colours = d.keys()
 	sizes = d.values()
 	colour_code = ["blue", "brown", "gold", "peachpuff", "lightslategray", "gray", "orange", "chocolate", "green",
 					"black", "sienna", "white", "peru", "moccasin", "tan", "red", "navajowhite"]
-					#green = tricolour
-	plt.pie(sizes,labels=colours, colors=colour_code, autopct='%1.1f%%')
-	plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+	plt.pie(sizes, labels=colours, colors=colour_code, autopct='%1.1f%%')
+	plt.axis('equal')
 	plt.show()
 	
 def top_pet_names(number):
 	"""
-	Returns top number of pet names
+	Returns top number of pet names.
 	"""
 	d = Counter(df["animal_nam"])
-	#print(d)
-	#print(d.items())
 	ranked = sorted(d.items(), key=lambda x: x[1], reverse=True)
 	most_common = ranked[:number]
 	remaining = 0
@@ -109,6 +116,9 @@ def top_pet_names(number):
 	plt.show()
 
 def top_dogcat_names(number):
+	"""
+	Plots number of dog/cat names in a pie chart.
+	"""
 	d_dog = Counter()
 	d_cat = Counter()
 	global_count = 0
@@ -121,8 +131,6 @@ def top_dogcat_names(number):
 		global_count += 1
 	dog_common = d_dog.most_common(number)
 	cat_common = d_cat.most_common(number)
-	print(dog_common)
-	print(cat_common)
 	plt.pie([float(i[1]) for i in dog_common], labels=[i[0] for i in dog_common], autopct='%1.1f%%')
 	plt.title("Top %d dog names" % number)
 	plt.axis('equal')
@@ -132,20 +140,17 @@ def top_dogcat_names(number):
 	plt.axis('equal')
 	plt.show()
 
+L = []
+for i in df["animal_nam"]:
+	if isinstance(i, float): continue
+	L.append((len(i), i))
+for i,j in sorted(L):
+	print(i, j)
 # BASICS #
-display_age_graph()
-pet_count_suburbs()
-colours()
-registered_or_not()
-dogs_v_cats()
-top_pet_names(5)
-top_dogcat_names(5)
-
-
-"""
-first talk about data set, its columns, how data was inputted (eg lots of consistnet inputs so probably a dropdown enu except for name
-)
-
-
-
-
+# display_age_graph()
+# pet_count_suburbs()
+# colours()
+# registered_or_not()
+# dogs_v_cats()
+# top_pet_names(5)
+# top_dogcat_names(5)
